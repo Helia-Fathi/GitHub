27b95f0577693eafc417cd4e0ae371b06b27b225
Helia <helia.f76@gmail.com>
2022-04-16 19:32:09 +0300

    feat: add git API

---
+++ ScalioTest/Networking/GitAPI.swift
@@ -0,0 +1,45 @@
+//
+//  GitAPI.swift
+//  ScalioTest
+//
+//  Created by Helia Fathi on 4/16/22.
+//
+
+import Foundation
+import Moya
+
+enum GitHub {
+    case searchUsers(query: String, page: Int, pageSize: Int)
+}
+
+extension GitHub: TargetType {
+    var baseURL: URL {
+        URL(string: "https://api.github.com")!
+    }
+    
+    var path: String {
+        switch self {
+        case  .searchUsers(_, _, _):
+            return "/search/users"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .searchUsers(_, _, _):
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case let .searchUsers(query, page, pageSize):
+            return .requestParameters(parameters: ["q": query, "page": page, "per_page": pageSize], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+}
+
---
+++ ScalioTest/Networking/GitProfileParser.swift
@@ -0,0 +1,12 @@
+//
+//  GitProfileParser.swift
+//  ScalioTest
+//
+//  Created by Helia Fathi on 4/16/22.
+//
+
+import Foundation
+import RxSwift
+import Moya
+
+
